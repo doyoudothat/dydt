27066c30f0b98f78d3ab37e91d0f2675fbc9dfc5
DoYouDoThat <git@doyoudothat.co.uk>
2017-12-01 15:18:34 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,89 @@
+<head>
+	<title>Web Development and Design Services for Cornwall and beyond | Do You Do That?</title>
+	<link rel="stylesheet" href="font/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" id="dydy-css"  href="css/dydt.css" type="text/css" media="all" />
+	<link rel="apple-touch-icon" sizes="57x57" href="img/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="img/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="img/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="img/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="img/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="img/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="img/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+	<link rel="manifest" href="/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+</head>
+<body>
+<div class="dydt--gridlayout">
+	<div class="dydt-row menu">
+		<div class="logo"><img src="img/dydt-logo.png" height="150" width="170"/></div>
+		<div class="main-menu"><a href="#formula">Formula</a>&nbsp;/&nbsp;<a href="#about">About</a>&nbsp;/&nbsp;<a href="#contactus">Contact</a></div>
+	</div>
+	<div class="dydt-row hero"><div class="key"><h1>When I meet people I'm often asked<br/><span class="biggie">DO YOU DO THAT?</span><br/> Well, we do lots of things, so how can we help you?</h1></div></div>
+	
+	<div class="dydt-row service"><h2>Our (not so secret) formula DYDT = (B<sub>y</sub>+C<sub>t</sub>D<sub>p</sub>)/P<sub>h</sub><sup>2</sup></h2></div>
+	<a name="forumla"></a><div class="dydt-row buddy">
+		<img src="img/by.png" height="150" width="150" />
+		<p class="serv-title">BUDDY</p>
+		<p class="serv-desc">Need a spare pair of hands loading up content, doing social or processing images? Do you need a second shooter to help you on shoots. Let us help ease the burden.</p>
+	</div>
+	<div class="dydt-row consult">
+		<img src="img/ct.png" height="150" width="150" />
+		<p class="serv-title">CONSULT</p>
+		<p class="serv-desc">We offer pre-project services including undertaking Feasibility Studies and helping to write requirement documents for companies looking to start new projects.</p>
+	</div>
+	<div class="dydt-row develop">
+		<img src="img/dp.png" height="150" width="150" />
+		<p class="serv-title">DEVELOP</p>
+		<p class="serv-desc">We develop websites based on the Wordpress platform, working with small, medium and large companies and not just Cornwall or the UK to explore new opportunities online.</p>
+	</div>
+	<div class="dydt-row photograph">
+		<img src="img/ph.png" height="150" width="150" />
+		<p class="serv-title">PHOTOGRAPH</p>
+		<p class="serv-desc">From PR to commissioned shoots, we have everything to take and process images ready for publication. We also offer post-processing services to make images shine.</p>
+	</div>
+	<a name="about"></a>
+	<div class="dydt-row about">
+		<div><h3>ABOUT US</h3><p>Unlike this site, not everything is <strike>black</strike> grey and white. When I meet people, when they find out what I do or what my backgound is, they inevitably ask whether I do this, or do that? And in answering them, I tend to say I do a lot of things. Do you do that? has been created as my way of helping people get to where they want to go. Our ethos is to keep things simple.</p><p>Do you do that? Offers four distinct services, but we are not a jack of all trades organisation, the services are the outcome of over 20 years of having the privilege to work in each of the areas listed above. We don't have any clients at the moment, so it'll be kinda cool if you want to work with someone different and we've worked with lots of small, medium and large companies over the years, so we'd love to work with you on new and exciting ideas.</div>
+	</div>	
+	<a name="contactus"></a>
+	<div class="dydt-row contact">
+		<h4>CONTACT US TO FIND OUT MORE</h4>
+		<div class="contactlft">
+			<form id="contact-frm">
+				<ul>
+					<li><label for="contactnm">Your Name: </label><br/><input type="text" size="20" name="contactnm" /></li>
+					<li><label for="contacttel">Contact Phone: </label><br/><input type="text" size="20" name="contacttel" /></li>
+					<li><label for="contactem">Contact Email: </label><br/><input type="text" size="20" name="contactem" /></li>
+					<li><label for="contactbdy">Please describe your question or request for help</label><br/><textarea name="contactq"
+		 cols="50" rows="5"></textarea></li>
+		 			<li><input type="submit" class="subbut" name"submitfm" value="SUBMIT" /></li>
+				</ul>
+			</form>
+			
+		</div>
+		<div class="contactrgt">
+			<p>You can also email us at: hey@doyoudothat.co.uk</p>
+			<p>Call us on: <a href="tel:07757924420" title="Contact Number">07757 924420</a></p>
+		</div>	
+		
+	</div>
+	<div class="dydt-row social">
+		<div class="social-icons">
+			<a href="http://www.instagram.com/doyoudothatuk" target="_blank" aria-label="Instagram" title="We're on Instagram"><i class="fa fa-instagram"></i></a>
+			<a href="http://www.twitter.com/doyoudothatuk" target="_blank" aria-label="Twitter" title="We're on Twitter"><i class="fa fa-twitter"></i></a>
+			<a href="http://www.facebook.com/doyoudothatuk" target="_blank" aria-label="Facebook" title="We're on Facebook"><i class="fa fa-facebook"></i></a>
+		</div>
+	</div>
+	<div class="dydt-row copyright"><p>&copy;<?php echo date("Y"); ?> Do you do that? All rights reserved.</p></div>	
+</div>
+</body>
+</html>
+
